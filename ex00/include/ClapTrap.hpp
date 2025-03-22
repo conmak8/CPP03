c9f8679b52d50b9025101ce2f7626f95d9e64031
cmakario <cmakario@student.42.fr>
2025-03-22 09:51:01 +0100

    00 : start of header file

---
+++ ex00/include/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 09:43:36 by cmakario          #+#    #+#             */
+/*   Updated: 2025/03/22 09:47:29 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+
+class ClapTrap
+{
+private:
+		std::string	name;
+public:
+		ClapTrap(std::string name);
+};
+
+#endif // !CLAPTRAP_HPP
