7b743eb5367102ee199a62d920e634a20a90c43f
cmakario <cmakario@student.42.fr>
2025-03-23 09:54:41 +0100

    01: main add for testing

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,29 +6,38 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 11:22:32 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/22 14:22:47 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/23 09:53:41 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ClapTrap.hpp"
+#include "../include/ScavTrap.hpp"
+
 
 int main()
 {
-	ClapTrap cp("Markus");
-	// ClapTrap cp2;
+	// ClapTrap cp("Markus");
+	// // ClapTrap cp2;
 
-	cp.attack("Ceasar");
-	// std::cout << cp.energyPoints << std::endl;
-	cp.takeDamage(2);
-	cp.beRepaired(4);
-	cp.takeDamage(4);
-	// ClapTrap cp3(cp);
-	// std::cout << cp3.getEnergy() << "aaaa" << std::endl;
-	cp.attack("Neron");
-	// std::cout << cp3.getEnergy() << "bbbb" << std::endl;
-	// std::cout << cp.getEnergy() << "vvvv" << std::endl;
-	cp.beRepaired(5);
-	cp.takeDamage(24);
+	// cp.attack("Ceasar");
+	// // std::cout << cp.energyPoints << std::endl;
+	// cp.takeDamage(2);
+	// cp.beRepaired(4);
+	// cp.takeDamage(4);
+	// // ClapTrap cp3(cp);
+	// // std::cout << cp3.getEnergy() << "aaaa" << std::endl;
+	// cp.attack("Neron");
+	// // std::cout << cp3.getEnergy() << "bbbb" << std::endl;
+	// // std::cout << cp.getEnergy() << "vvvv" << std::endl;
+	// cp.beRepaired(5);
+	// cp.takeDamage(24);
 
-	return (0);
+// ----01: test
+	ScavTrap serena("Serena");
+	serena.attack("Loci");
+	serena.takeDamage(30);
+	serena.beRepaired(20);
+	serena.guardGate();
+
+	return 0;
 }
