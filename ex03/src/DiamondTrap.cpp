0e68628b2f571fc358e6e9c2373c247a38194f4a
cmakario <cmakario@student.42.fr>
2025-03-24 10:18:58 +0100

    03: correct constructors

--- ex03/src/DiamondTrap.cpp
+++ ex03/src/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 13:02:48 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/23 13:26:43 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/24 10:14:43 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,28 @@
 
 DiamondTrap::DiamondTrap() : ClapTrap("Unknown_clap_name")																		// * Constructor
 {
-	this->name = "Uknown";
-	FragTrap::hitPoints = 100;
-	ScavTrap::energyPoints = 50;
-	FragTrap::attackDamage = 30;
-	std::cout << CYAN << "🦿 DiamondTrap 🏗️  Constructor called for " << UBR << name << RESET << std::endl;
+	this->name = "Unknown";
+	hitPoints = FragTrap::hitPoints;
+	energyPoints = ScavTrap::energyPoints;
+	attackDamage = FragTrap::attackDamage;
+	std::cout << CYAN << "💎 DiamondTrap 🏗️  Constructor called for " << UBR << name << RESET << std::endl;
 }
 
 DiamondTrap::DiamondTrap(const std::string &name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name)				// * Constructor with parameters
 {
 	this->name = name;
-	FragTrap::hitPoints = 100;
-	ScavTrap::energyPoints = 50;
-	FragTrap::attackDamage = 30;
-	std::cout << CYAN << "🦿 DiamondTrap 🏗️  Constructor called for " << UBR << name << RESET << std::endl;
+	hitPoints = FragTrap::hitPoints;
+	energyPoints = ScavTrap::energyPoints;
+	attackDamage = FragTrap::attackDamage;
+	std::cout << CYAN << "💎 DiamondTrap 🏗️  Constructor called for " << UBR << name << RESET << std::endl;
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap &copy) : ClapTrap(copy) 																// * Copy constructor
+DiamondTrap::DiamondTrap(const DiamondTrap &copy) : ClapTrap(copy), ScavTrap(copy), FragTrap(copy)								// * Copy constructor
 {
-	std::cout << CYAN << "🦿 DiamondTrap 🏗️  Copy Constructor called for " << UBR << name << RESET << std::endl;
+	std::cout << CYAN << "💎 DiamondTrap 🏗️  Copy Constructor called for " << UBR << name << RESET << std::endl;
 }
 
+//  Todo : not sure how to handle this one
 DiamondTrap &DiamondTrap::operator= (const DiamondTrap &copy)																	// * Copy assignment operator
 {
 	if (this == &copy)
@@ -44,29 +45,27 @@ DiamondTrap &DiamondTrap::operator= (const DiamondTrap &copy)																	//
 		return (*this);
 	}
 	ClapTrap::operator=(copy);
-	std::cout << CYAN << "🦿 DiamondTrap 🏗️  Copy Assignment operator called for " << UBR << name << RESET << std::endl;
+	name = copy.name;				// ! check again what happens here!
+	// ClapTrap::name = copy.ClapTrap::name;
+	// hitPoints = copy.hitPoints;
+	// energyPoints = copy.energyPoints;
+	// attackDamage = copy.attackDamage;
+	
+	std::cout << CYAN << "💎 DiamondTrap 🏗️  Copy Assignment operator called for " << UBR << name << RESET << std::endl;
 	return (*this);
 }
 
-DiamondTrap::~DiamondTrap()																									// * Destructor
+DiamondTrap::~DiamondTrap()																										// * Destructor
 {
-	std::cout << RED << "🦿 DiamondTrap 🧨 Destructor called for " << UBR << name << RESET << std::endl;
+	std::cout << RED << "💎 DiamondTrap 🧨 Destructor called for " << UBR << name << RESET << std::endl;
 }
 
 void DiamondTrap::attack(const std::string &target)
 {
-	if ((hitPoints <= 0) || (energyPoints <= 0))
-	{
-		std::cout << "🦿  DiamondTrap unable to attack " << target << " due to low energy points or hit points!" << std:: endl;
-		return;
-	}
-	energyPoints--;
-	std::cout << YELLOW << "🦿 DiamondTrap " << UBR << name << RESET << YELLOW << " attacks " << target << " causing " << BOLD << attackDamage << RESET << YELLOW << " points of damage!💥" << RESET << std::endl;
-	std::cout << "❤️  HP left : " << hitPoints << std::endl;
-	std::cout << "⚡️ EP left : " << energyPoints << std::endl;
+	ScavTrap::attack(target);
 }
 
-// void DiamondTrap::whoAmI()
-// {
-// 	std::cout << CYAN << "🦿 DiamondTrap "<< name << " in now politely asking 🙌 otherwise 💨" << RESET << std:: endl;
-// }
+void DiamondTrap::whoAmI()
+{
+	std::cout << CYAN << "💎 DiamondTrap name is : " << name << " | 🪤  ClapTrap name is : " << ClapTrap::name << RESET << std:: endl;
+}
